1764866fdffc8440290e369ab621644ad3167dde
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2023-01-18 22:42:38 +0300

    Format

--- content/content.js
+++ content/content.js
@@ -1,102 +1,104 @@
 chrome.runtime.sendMessage({
-    from: "content",
-    subject: "showPageAction",
+  from: "content",
+  subject: "showPageAction",
 });
 
 function draggable(el) {
-    el.addEventListener('mousedown', function (e) {
-        console.log(e.target);
-        if (e.target.tagName === "INPUT") {
-            return;
-        }
-        const offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
-        const offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
+  el.addEventListener("mousedown", function (e) {
+    if (e.target.tagName === "INPUT") {
+      return;
+    }
+    const offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+    const offsetY = e.clientY - parseInt(window.getComputedStyle(this).top);
 
-        function mouseMoveHandler(e) {
-            e.preventDefault();
-            el.style.cursor = "grabbing";
-            el.style.top = (e.clientY - offsetY) + 'px';
-            el.style.left = (e.clientX - offsetX) + 'px';
-        }
+    function mouseMoveHandler(e) {
+      e.preventDefault();
+      el.style.cursor = "grabbing";
+      el.style.top = e.clientY - offsetY + "px";
+      el.style.left = e.clientX - offsetX + "px";
+    }
 
-        function reset() {
-            window.removeEventListener('mousemove', mouseMoveHandler);
-            window.removeEventListener('mouseup', reset);
-            el.style.cursor = "grab";
-        }
+    function reset() {
+      window.removeEventListener("mousemove", mouseMoveHandler);
+      window.removeEventListener("mouseup", reset);
+      el.style.cursor = "grab";
+    }
 
-        window.addEventListener('mousemove', mouseMoveHandler);
-        window.addEventListener('mouseup', reset);
-    });
+    window.addEventListener("mousemove", mouseMoveHandler);
+    window.addEventListener("mouseup", reset);
+  });
 }
 
 function createOverlay(screenImageData) {
-    const overlay = document.createElement("div");
-    overlay.classList.add("zeplin-extension-overlay");
+  const overlay = document.createElement("div");
+  overlay.classList.add("zeplin-extension-overlay");
 
-    const { src, width, height } = screenImageData;
+  const { src, width, height } = screenImageData;
 
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("zeplin-extension-wrapper");
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("zeplin-extension-wrapper");
 
-    const header = document.createElement("div");
-    header.classList.add("zeplin-extension-header");
+  const header = document.createElement("div");
+  header.classList.add("zeplin-extension-header");
 
-    const slider = document.createElement("input");
-    slider.type = "range";
-    slider.min = 10
-    slider.max = 100
-    slider.value = 70
-    slider.classList.add("zeplin-extension-slider");
-    slider.oninput = event => {
-        image.style.opacity = event.target.value / 100
-    }
-    header.append(slider);
+  const slider = document.createElement("input");
+  slider.type = "range";
+  slider.min = 10;
+  slider.max = 100;
+  slider.value = 70;
+  slider.classList.add("zeplin-extension-slider");
+  slider.oninput = (event) => {
+    image.style.opacity = event.target.value / 100;
+  };
+  header.append(slider);
 
-    const closeButton = document.createElement("button");
-    closeButton.textContent = "x";
-    closeButton.classList.add("zeplin-extension-close-button");
-    closeButton.addEventListener("click", () => {
-        overlay.remove();
-    });
-    header.append(closeButton);
-    wrapper.append(header);
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "x";
+  closeButton.classList.add("zeplin-extension-close-button");
+  closeButton.addEventListener("click", () => {
+    overlay.remove();
+  });
+  header.append(closeButton);
+  wrapper.append(header);
 
-    const image = document.createElement("img");
-    image.classList.add("zeplin-extension-image");
-    image.src = src;
-    image.width = width;
-    image.height = height;
-    image.style.opacity = 0.7;
-    wrapper.append(image);
+  const image = document.createElement("img");
+  image.classList.add("zeplin-extension-image");
+  image.src = src;
+  image.width = width;
+  image.height = height;
+  image.style.opacity = 0.7;
+  wrapper.append(image);
 
-    overlay.append(wrapper);
-    draggable(wrapper);
+  overlay.append(wrapper);
+  draggable(wrapper);
 
-    return overlay;
+  return overlay;
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if (msg.from === "popup") {
-        if (msg.subject === "DOMInfo") {
-            const screenImage = document.querySelector(".snapshotImage");
-            console.log("si", screenImage)
-            if (screenImage) {
-                response({
-                    src: screenImage.src,
-                    width: screenImage.getAttribute("width"),
-                    height: screenImage.getAttribute("height")
-                })
-            }
-        } else if (msg.subject === "compare") {
-            chrome.storage.sync.get("screenImageData", function (data) {
-                if (data && data.screenImageData && !document.querySelector(".zeplin-extension-overlay")) {
-                    const overlay = createOverlay(data.screenImageData);
-                    document.body.append(overlay);
-                }
-            });
+  if (msg.from === "popup") {
+    if (msg.subject === "DOMInfo") {
+      const screenImage = document.querySelector(".snapshotImage");
+      if (screenImage) {
+        response({
+          src: screenImage.src,
+          width: screenImage.getAttribute("width"),
+          height: screenImage.getAttribute("height"),
+        });
+      }
+    } else if (msg.subject === "compare") {
+      chrome.storage.sync.get("screenImageData", function (data) {
+        if (
+          data &&
+          data.screenImageData &&
+          !document.querySelector(".zeplin-extension-overlay")
+        ) {
+          const overlay = createOverlay(data.screenImageData);
+          document.body.append(overlay);
         }
+      });
     }
+  }
 
-    return true
+  return true;
 });
--- popup/popup.js
+++ popup/popup.js
@@ -4,53 +4,65 @@ const popupImage = document.querySelector("#image");
 const popupButton = document.querySelector("#button");
 
 function showScreenDetail() {
-    screenDetail.classList.remove("hidden")
-    info.classList.add("hidden")
+  screenDetail.classList.remove("hidden");
+  info.classList.add("hidden");
 }
 
 function showInfo() {
-    screenDetail.classList.add("hidden")
-    info.classList.remove("hidden")
+  screenDetail.classList.add("hidden");
+  info.classList.remove("hidden");
 }
 
 // Chrome Events
 chrome.storage.sync.get("screenImageData", function (data) {
-    if (data && data.screenImageData) {
-        showScreenDetail();
+  if (data && data.screenImageData) {
+    showScreenDetail();
 
-        popupImage.src = data.screenImageData.src
-    } else {
-        showInfo();
-    }
+    popupImage.src = data.screenImageData.src;
+  } else {
+    showInfo();
+  }
 });
 // END - Chrome Events
 
 // DOM Events
 popupButton.addEventListener("click", () => {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-    }, tabs => {
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            { from: 'popup', subject: 'compare' });
-    });
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        from: "popup",
+        subject: "compare",
+      });
+
+      window.close();
+    }
+  );
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, { from: 'popup', subject: 'DOMInfo' },
-            data => {
-                if (data) {
-                    showScreenDetail();
+window.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { from: "popup", subject: "DOMInfo" },
+        (data) => {
+          if (data) {
+            showScreenDetail();
 
-                    popupImage.src = data.src
-                    chrome.storage.sync.set({ "screenImageData": data });
-                }
-            });
-    });
+            popupImage.src = data.src;
+            chrome.storage.sync.set({ screenImageData: data });
+          }
+        }
+      );
+    }
+  );
 });
 // END - DOM Events
